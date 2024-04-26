e64dfd5a9ba2ba2ba021d7fc941193ba36b4133d
estelif <230682@astanait.edu.kz>
2024-04-27 01:01:44 +0500

    all functions of LinkedList

---
+++ src/MyLinkedList.java
@@ -0,0 +1,204 @@
+import java.util.Iterator;
+
+public class MyLinkedList<T> implements MyList<T> {
+
+
+    private static class MyNode<T> {
+        private T element;
+        private MyNode<T> next;
+        private MyNode<T> prev;
+
+        public MyNode(T element) {
+            this.element = element;
+        }
+    }
+
+    private MyNode<T> head;
+    private MyNode<T> tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public void add(T item) {
+        MyNode<T> newNode = new MyNode<>(item);
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+        size++;
+    }
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("index not correct");
+    }
+
+    @Override
+    public void set(int index, T item) {
+        checkIndex(index);
+        MyNode<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        current.element = item;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        checkIndex(index);
+        if (index == 0) {
+            addFirst(item);
+        } else if (index == size) {
+            addLast(item);
+        } else {
+            MyNode<T> newNode = new MyNode<>(item);
+            MyNode<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next.prev = newNode;
+            newNode.prev = current;
+            current.next = newNode;
+            size++;
+        }
+    }
+
+    @Override
+    public void addFirst(T item) {
+        MyNode<T> newNode = new MyNode<>(item);
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+        size++;
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(item);
+    }
+
+    @Override
+    public T get(int index) {
+        checkIndex(index);
+        MyNode<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.element;
+    }
+
+    @Override
+    public T getFirst() {
+        return head.element;
+    }
+
+    @Override
+    public T getLast() {
+        return tail.element;
+    }
+
+    @Override
+    public void remove(int index) {
+        checkIndex(index);
+
+        if (size == 1) {
+            head = tail = null;
+        } else if (index == 0) {
+            removeFirst();
+        } else if (index == size - 1) {
+            removeLast();
+        } else {
+            MyNode<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+            current.next.prev = current;
+            size--;
+        }
+    }
+
+    @Override
+    public void removeFirst() {
+        if (size == 1) {
+            head = tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+        }
+        size--;
+    }
+    @Override
+    public void removeLast() {
+        if (size == 1){
+            head = tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+        }
+        size--;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        MyNode<T> current = head;
+        int index = 0;
+        while (current != null) {
+            if (current.element.equals(object)) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        MyNode<T> current = tail;
+        int index = size - 1;
+        while (current != null) {
+            if (current.element.equals(object)) {
+                return index;
+            }
+            current = current.prev;
+            index--;
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public void clear() {
+        head = tail = null;
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
