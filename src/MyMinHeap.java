bc66937ed8c2eb98e258e372ccfd3f35906ef172
estelif <230682@astanait.edu.kz>
2024-04-29 14:45:36 +0500

    implementations1

--- src/MyMinHeap.java
+++ src/MyMinHeap.java
@@ -1,44 +1,51 @@
+/**
+ * This class represents a custom implementation of a minimum heap data structure.
+ * It internally uses MyArrayList for storing elements.
+ * The class provides methods for heap operations such as insertion, extraction of minimum,
+ * getting the minimum element, and heapifying to maintain the heap property.
+ */
+
 public class MyMinHeap<T extends Comparable<T>> {
-    MyArrayList<T> heap = new MyArrayList<>();   //MyArrayList
+    MyArrayList<T> minheap = new MyArrayList<>();
 
     public MyMinHeap(){
     }
 
     public int size(){
-        return heap.size();
+        return minheap.size();
     }
 
     public void clear(){
-        heap.clear();
+        minheap.clear();
     }                          // all elements will be deleted from heap
 
     public T getMin(){
-        return heap.getFirst();
-    }                          //1st element get
+        return minheap.getFirst();
+    }                          //first element get
 
 
     public T extractMin(){
-        T min = heap.getFirst();
-        swap(0, heap.size()-1);
-        heap.removeLast();
+        T min = minheap.getFirst();
+        swap(0, minheap.size()-1);
+        minheap.removeLast();
         heapify(0);
         return min;
     }                       // Gets minimum and swaps with last element and removes last element
 
     public void insert(T item){
-        heap.add(item);
-        int i = heap.size() - 1;
+        minheap.add(item);
+        int i = minheap.size() - 1;
         heapify(i);
-    }                      // new item will insert at the end of heap
+    }                      // inserts new item at the end of heap
 
     private void heapify(int i){
         int left = leftChildOf(i);
         int right = rightChildOf(i);
         int minimum = i;
-        if (left < heap.size() && heap.get(left).compareTo(heap.get(minimum)) < 0){
+        if (left < minheap.size() && minheap.get(left).compareTo(minheap.get(minimum)) < 0){
             minimum = left;
         }
-        if (right < heap.size() && heap.get(right).compareTo(heap.get(minimum)) < 0){
+        if (right < minheap.size() && minheap.get(right).compareTo(minheap.get(minimum)) < 0){
             minimum = right;
         }
         if (minimum != i){
@@ -48,24 +55,24 @@ public class MyMinHeap<T extends Comparable<T>> {
     }                        // heapify function to maintain the heap functions after deletion or insertion
 
     private void traverse(int i){
-        if (i < heap.size()){
-            System.out.println(heap.get(i));
+        if (i < minheap.size()){
+            System.out.println(minheap.get(i));
             traverse(leftChildOf(i));
             traverse(rightChildOf(i));
         }
-    }              // traverse function to print the heap
+    }              // print the heap
     public int leftChildOf(int i){
         return 2 * i;
-    }                 //left child
+    }                 //returns left child
     public int rightChildOf(int i){
         return 2 * i + 1;
-    }                  //right child
+    }                  //returns right child
     public int parentOf(int i){
         return i / 2;
-    }                // Returns parent
-    private void swap(int i, int j) {
-        T temp = heap.get(i);
-        heap.set(i, heap.get(j));
-        heap.set(j, temp);
+    }                // returns parent
+    private void swap(int k, int z) {
+        T temp = minheap.get(k);
+        minheap.set(k, minheap.get(z));
+        minheap.set(z, temp);
     }               // swapping of two elements
 }
